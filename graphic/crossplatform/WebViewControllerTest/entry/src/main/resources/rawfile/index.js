3b2d9d9e9af417e077bc6b6c68e21cace869720d
quguiren <quguiren@huawei.com>
2025-01-24 15:09:08 +0800

    fix code check
    Signed-off-by: quguiren <quguiren@huawei.com>

--- graphic/crossplatform/WebViewControllerTest/entry/src/main/resources/rawfile/index.js
+++ graphic/crossplatform/WebViewControllerTest/entry/src/main/resources/rawfile/index.js
@@ -1,54 +1,54 @@
-//index.js
-var h5Port;
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const decoder = new TextDecoder('utf-8');
 
-// 使用h5Port往ets侧发送String类型的消息.
-function postStringToEts(message) {
-    if (h5Port) {
-        h5Port.postMessage(message);
-    } else {
-        console.error("In html h5port is null, please init first");
-    }
-}
-
-window.addEventListener('message', function(event) {
-    if (event.data == 'init_web_messageport') {
-        if(event.ports[0] != null) {
-            h5Port = event.ports[0]; // 1. 保存从ets侧发送过来的端口
-            h5Port.onmessage = function(event) {
-                console.log("hwd In html got message");
-                // 2. 接收ets侧发送过来的消息.
+window.addEventListener('message', (event) => {
+    if (event.data === 'init_web_messageport') {
+        if (event.ports[0] !== null) {
+            const [h5Port] = event.ports;
+            h5Port.onmessage = (event) => {
                 var result = event.data;
-                console.log("In html got message, typeof: ", typeof(result));
-                console.log("In html got message, result: ", (result));
-                if (typeof(result) == "string") {
-                    document.getElementById("msg").innerHTML = "String:" + result;
-                    postStringToEts("String:" + result);
-                } else if (typeof(result) == "number") {
-                    document.getElementById("msg").innerHTML = "Number:" + result;
-                    postStringToEts("Number:" + result);
-                } else if (typeof(result) == "boolean") {
-                    document.getElementById("msg").innerHTML = "Boolean:" + result;
-                    postStringToEts("Boolean:" + result);
-                } else if (typeof(result) == "object") {
+                if (typeof(result) === 'string') {
+                    document.getElementById('msg').innerHTML = `String:${result}`;
+                    h5Port.postMessage(`String:${result}`);
+                } else if (typeof(result) === 'number') {
+                    document.getElementById('msg').innerHTML = `Number:${result}`;
+                    h5Port.postMessage(`Number:${result}`);
+                } else if (typeof(result) === 'boolean') {
+                    document.getElementById('msg').innerHTML = `Boolean:${result}`;
+                    h5Port.postMessage(`Boolean:${result}`);
+                } else if (typeof(result) === 'object') {
                     if (result instanceof Error) {
-                        document.getElementById("msg2").innerHTML = "Error:" + result.name + ", msg:" + result.message;
-                        postStringToEts("Error:" + result.name);
+                        document.getElementById('msg2').innerHTML = `Error:${result.name}, msg:${result.message}`;
+                        h5Port.postMessage(`Error:${result.name}`);
                     } else if (result instanceof Array) {
-                        document.getElementById("msg2").innerHTML = "Array len:" + result.length + ", value:" + result;
-                        postStringToEts("Array:" + result);
+                        document.getElementById('msg2').innerHTML = `Array len:${result.length}, value:${result}`;
+                        h5Port.postMessage(`Array:${result}`);
                     } else {
-                        document.getElementById("msg").innerHTML = "not any instance of support type";
-                        postStringToEts("not support type");
+                        document.getElementById('msg').innerHTML = 'not any instance of support type';
+                        h5Port.postMessage('not support type');
                     }
                 } else {
-                    document.getElementById("msg").innerHTML = "not support type";
-                    postStringToEts("not support type");
+                    document.getElementById('msg').innerHTML = 'not support type';
+                    h5Port.postMessage('not support type');
                 }
             }
             h5Port.onmessageerror = (event) => {
-                postStringToEts(`html Error:${event}`);
+                h5Port.postMessage(`html Error:${event}`);
             };
         }
     }
-})
+});
--- graphic/crossplatform/WebViewControllerTest/entry/src/main/resources/rawfile/index_on.js
+++ graphic/crossplatform/WebViewControllerTest/entry/src/main/resources/rawfile/index_on.js
@@ -1,53 +1,53 @@
-//index_on.js
-var h5Port;
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const encoder = new TextEncoder();
 
-// 使用h5Port往ets侧发送消息.
-function postStringToEts(message) {
-    if (h5Port) {
-        h5Port.postMessage(message);
-    } else {
-        console.error("In html h5port is null, please init first");
-    }
-}
-
-window.addEventListener('message', function(event) {
-    if (event.data == 'init_web_messageport') {
-        if(event.ports[0] != null) {
-            h5Port = event.ports[0]; // 1. 保存从ets侧发送过来的端口
-            h5Port.onmessage = function(event) {
-                console.log("hwd In html got message");
-                // 2. 接收ets侧发送过来的消息.
-                var result = event.data;
-                console.log("In html got message, typeof: ", typeof(result));
-                console.log("In html got message, result: ", (result));
-                if (typeof(result) == "string") {
-                    // 根据收到的信息，发送相应类型的数据
-                    document.getElementById("msg").innerHTML = "String:" + result;
-                    if (result == "string") {
-                        postStringToEts("helloFromH5");
-                    } else if (result == "number") {
-                        postStringToEts(54321);
-                    } else if (result == "boolean") {
-                        postStringToEts(false);
-                    } else if (result == "array") {
-                        postStringToEts([3, 2, 1]);
-                    } else if (result == "error") {
+window.addEventListener('message', (event) => {
+    if (event.data === 'init_web_messageport') {
+        if (event.ports[0] !== null) {
+            const [h5Port] = event.ports;
+            h5Port.onmessage = (event) => {
+                let result = event.data;
+                if (typeof(result) === 'string') {
+                    document.getElementById('msg').innerHTML = `String:${result}`;
+                    if (result === 'string') {
+                        h5Port.postMessage('helloFromH5');
+                    } else if (result === 'number') {
+                        h5Port.postMessage(54321);
+                    } else if (result === 'boolean') {
+                        h5Port.postMessage(false);
+                    } else if (result === 'array') {
+                        h5Port.postMessage([3, 2, 1]);
+                    } else if (result === 'error') {
                         try {
-                            throw new Error("This is Error from H5");
+                            throw new Error('This is Error from H5');
                         } catch (error) {
-                            postStringToEts(error);
+                            h5Port.postMessage(error);
                         }
+                    } else {
+                        h5Port.postMessage('not support this type');
                     }
-
                 } else {
-                    document.getElementById("msg").innerHTML = "incorrect type";
-                    postStringToEts("incorrect type");
+                    document.getElementById('msg').innerHTML = 'incorrect type';
+                    h5Port.postMessage('incorrect type');
                 }
             }
             h5Port.onmessageerror = (event) => {
-                postStringToEts(`html Error:${event}`);
+                h5Port.postMessage(`html Error:${event}`);
             };
         }
     }
-})
+});
