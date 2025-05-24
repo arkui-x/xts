f34a57813d968b14f47ccf184dcfb055411e48e6
quguiren <quguiren1@huawei-partners.com>
2025-05-24 17:28:07 +0800

    fix bundle name
    Signed-off-by: quguiren <quguiren1@huawei-partners.com>

--- commonlibrary/crossplatform/sendableCollections/.arkui-x/ios/app/AppDelegate.m
+++ commonlibrary/crossplatform/sendableCollections/.arkui-x/ios/app/AppDelegate.m
@@ -18,7 +18,7 @@
 #import <libarkui_ios/StageApplication.h>
 
 #define BUNDLE_DIRECTORY @"arkui-x"
-#define BUNDLE_NAME @"com.example.test"
+#define BUNDLE_NAME @"com.example.sendablecollections"
 
 @interface AppDelegate ()
 
