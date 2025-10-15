e0048529eac06ae6b7a4975732bb5915aae1c3e1
jinhaiOH5 <jinhai2@huawei.com>
2025-10-15 11:21:23 +0800

    [Master][ArkUI] Xcomponent  testcase bug fix
    Signed-off-by: jinhaiOH5 <jinhai2@huawei.com>

--- arkui/crossplatform/Xcomponent/.arkui-x/ios/app/AppDelegate.m
+++ arkui/crossplatform/Xcomponent/.arkui-x/ios/app/AppDelegate.m
@@ -18,7 +18,7 @@
 #import <libarkui_ios/StageApplication.h>
 
 #define BUNDLE_DIRECTORY @"arkui-x"
-#define BUNDLE_NAME @"packageName"
+#define BUNDLE_NAME @"com.example.xcomponent"
 
 @interface AppDelegate ()
 
